9f4e7c4b26727be9d2680b2741565045bc59666c
Jordon Frank <Maudemoo@gmail.com>
2018-06-12 21:51:24 -0700

    forgot to save register form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -12,26 +12,20 @@ class RegisterForm extends Component {
           <b>Email</b>
         </label>
         <input className="login-input" type="text" placeholder="Enter Username"  required="required"/>
-
         <label for="psw">
           <b>Password</b>
         </label>
         <input className="login-input" type="password" placeholder="Enter Password"  required="required"/>
-
           <label for="name">
             <b>Full Name</b>
           </label>
           <input className="login-input" type="text" placeholder="Enter Name"  required="required"/>
-
             <label for="image_url">
               <b>Image Url</b>
             </label>
             <input className="login-input" type="text" placeholder="Enter an image url..." name="image_url" required="required"/>
-
         <Link to="/"><button type="submit">Register</button></Link>
-
       </div>
-
       <div class="container">
         <button type="button" onClick={()=>this.props.toggleRegister} class="cancelbtn">Cancel</button>
         <span class="psw">Forgot
